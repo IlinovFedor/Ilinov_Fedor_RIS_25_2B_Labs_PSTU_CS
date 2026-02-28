2e7a5f9ef20433d82b798e3c17e267de911a63aa
IlinovFedor <ilinov_f@mail.ru>
2026-03-01 01:29:41 +0500

    fix lab8, lab9, init lab10

--- sem2/lab9/main.cpp
+++ sem2/lab9/main.cpp
@@ -1,59 +1,75 @@
-#include <fstream>
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 ifstream f1("F1");
 ofstream f2("F2");
-string str;
+string ans;
+int ansCnt = 0;
 
-string ans = "";
-int ansCnt;
+string words[1000];
+int amounts[1000];
+int cnt;
+
+int countA(string& word) {
+    int cntA = 0;
+    for (int i = 0; i < word.size(); i++) {
+        if (int(word[i]) == -48 && (int(word[i + 1]) == -112 || int(word[i + 1]) == -80))
+            cntA++;
+    }
+    return cntA;
+}
+
+void saveWord(string& word, int cntA) {
+    if (cntA > ansCnt) {
+        ansCnt = cntA;
+        ans = word;
+    }
+    words[cnt] = word;
+    cnt++;
+    word = "";
+}
+
+void parseLine(string& str) {
+    cnt = 0;
+    string word;
+
+    for (int i = 0; i < str.size(); i++) {
+        if (!(32 <= str[i] && str[i] <= 64)) {
+            word += str[i];
+        } else if (!word.empty()) {
+            saveWord(word, countA(word));
+        }
+    }
+    if (!word.empty())
+        saveWord(word, countA(word));
+}
+
+void countAmounts() {
+    for (int i = 0; i < cnt; i++) {
+        amounts[i] = 1;
+        for (int j = i + 1; j < cnt; j++) {
+            if (!words[i].empty() && words[i] == words[j]) {
+                amounts[i]++;
+                words[j] = "";
+            }
+        }
+    }
+}
+
+bool hasWordTwice() {
+    for (int i = 0; i < cnt; i++)
+        if (amounts[i] == 2) return true;
+    return false;
+}
 
 int main() {
+    string str;
     while (getline(f1, str)) {
-        string word;
-        string words[1000];
-        int amounts[1000] = {};
-        int cntA = 0;
-
-        int cnt = 0;
-        for (int i = 0; i < str.size(); i++) {
-            if (!(32 <= str[i] && str[i] <= 64)) {
-                word += str[i];
-                if (int(str[i]) == -48 && (int(str[i + 1]) == -112 || int(str[i + 1]) == -80))
-                    cntA++;
-            }
-            else if (!word.empty()) {
-                if (cntA > ansCnt) {
-                    ansCnt = cntA;
-                    ans = word;
-                    cout << word << "A\n";
-                }
-                words[cnt] = word;
-                cnt++;
-                word = "";
-                cntA = 0;
-            }
-        }
-
-        for (int i = 0; i < cnt; i++) {
-            amounts[i] = 1;
-            for (int j = i + 1; j < cnt; j++) {
-                if (i != j && !words[i].empty() && words[i] == words[j]) {
-                    amounts[i]++;
-                    words[j] = "";
-                }
-            }
-        }
-
-        bool f = true;
-        for (int i = 0; i < cnt && f; i++) {
-            if (amounts[i] == 2) {
-                f2 << str << '\n';
-                f = false;
-            }
-        }
+        parseLine(str);
+        countAmounts();
+        if (hasWordTwice())
+            f2 << str << '\n';
     }
     f2 << '\n' << ans;
 }
