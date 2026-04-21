5b5cf9fb8d323c033d7fb375ca9357179aec1def
IlinovFedor <ilinov_f@mail.ru>
2026-04-21 23:00:25 +0500

    difficult_searches code+blockscheme

--- sem2/homeworks_2/difficult_searches/boyer_moor.cpp
+++ sem2/homeworks_2/difficult_searches/boyer_moor.cpp
@@ -3,22 +3,33 @@
 
 int boyerMoor(const std::string &s, const std::string &k) {
     std::cout << "boyer-moor\n";
-    int cnt = 0;
-    for (int i = 0; i < s.size() - k.size(); i++) {
-        bool f = true;
-        for (int j = i; j < i + k.size() && f; j++) {
-            if (s[j] != k[j - i]) f = false;
-            cnt++;
+    int table[256];
+    for (auto &i: table) i = (int) k.length();
+    for (int i = 0; i < (int)k.length() - 1; i++)
+        table[(unsigned char) k[i]] = (int)k.length() - i - 1;
+
+    for (int i = 0; i < 256; i++)
+        if (table[i] != (int) k.length())
+            std::cout << '\t' << (char) i << ": " << table[i] << '\n';
+    std::cout << '\t' << "others symbols: " << k.length() << "\n\n";
+    std::cout << '\t' << s << '\n';
+    std::cout << '\t' << k << '\n';
+
+    int i = (int)k.length() - 1;
+    while (i < (int)s.length()) {
+        int j = (int)k.length() - 1;
+        while (0 <= j && k[j] == s[i - ((int)k.length() - 1 - j)])
+            j--;
+        if (j == -1) {
+            std::cout << "\tfound on index = " << i - (int)k.length() + 1 << '\n';
+            return i - (int)k.length() + 1;
         }
-        if (f) {
-            std::cout << "\tfound on index = " << i << '\n';
-            std::cout << "\tsteps: " << cnt << '\n';
-            std::cout << "\tboyer-moor\n";
-            return i;
-        }
+        int bad_char_pos = i - ((int)k.length() - 1 - j);
+        i += table[(unsigned char)s[bad_char_pos]];
+
+        std::cout << '\t';
+        for (int x = 0; x < i - (int)k.length() + 1; x++) std::cout << ' ';
+        std::cout << k << '\n';
     }
-    std::cout << "\tnothing was found\n";
-    std::cout << "\tsteps: " << cnt << '\n';
-    std::cout << "boyer-moor\n";
     return -1;
 }
--- sem2/homeworks_2/difficult_searches/naive.cpp
+++ sem2/homeworks_2/difficult_searches/naive.cpp
@@ -4,21 +4,19 @@
 int naive(const std::string &s, const std::string &k) {
     std::cout << "naive\n";
     int cnt = 0;
-    for (int i = 0; i < s.size() - k.size(); i++) {
+    for (int i = 0; i <= s.length() - k.length(); i++) {
         bool f = true;
-        for (int j = i; j < i + k.size() && f; j++) {
+        for (int j = i; j < i + k.length() && f; j++) {
             if (s[j] != k[j - i]) f = false;
             cnt++;
         }
         if (f) {
-            std::cout << "\tfound on index = " << i << '\n';
             std::cout << "\tsteps: " << cnt << '\n';
-            std::cout << "\tnaive\n";
+            std::cout << "\tfound on index = " << i << '\n';
             return i;
         }
     }
     std::cout << "\tnothing was found\n";
     std::cout << "\tsteps: " << cnt << '\n';
-    std::cout << "naive\n";
     return -1;
 }
