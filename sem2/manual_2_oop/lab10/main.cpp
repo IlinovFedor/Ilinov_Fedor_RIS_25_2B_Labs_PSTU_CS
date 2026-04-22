046143acc5dc98154d932fa48a186520b9ff05d0
IlinovFedor <ilinov_f@mail.ru>
2026-04-22 15:01:03 +0500

    oop10

--- sem2/manual_2_oop/lab10/main.cpp
+++ sem2/manual_2_oop/lab10/main.cpp
@@ -1,93 +1,85 @@
-#include "Pair.h"
+#include "gpt_cal/Pair.h"
 #include "file_work.h"
 #include <iostream>
 
-using namespace std;
-
 int main() {
+    std::string file_name;
     int c = 0;
-    char file_name[30];
     do {
-        cout << "\n1. Make file";
-        cout << "\n2. Print file";
-        cout << "\n3. Delete records less than key";
-        cout << "\n4. Increase records equal to key";
-        cout << "\n5. Add K records after N";
-        cout << "\n0. Exit\n";
-        cin >> c;
+        std::cout << "\n1. Make file";
+        std::cout << "\n2. Print file";
+        std::cout << "\n3. Delete records less than key";
+        std::cout << "\n4. Increase records equal to key";
+        std::cout << "\n5. Add K records after N";
+        std::cout << "\n0. Exit\n";
+        std::cin >> c;
         switch (c) {
             case 1: {
-                cout << "file name?";
-                cin >> file_name;
+                std::cout << "file name? ";
+                std::cin >> file_name;
                 int k = make_file(file_name);
-                if (k < 0) {
-                    cout << "Can't make file";
-                }
+                if (k < 0) std::cout << "Can't make file\n";
+
                 break;
             }
             case 2: {
-                cout << "file name?";
-                cin >> file_name;
+                std::cout << "file name? ";
+                std::cin >> file_name;
                 int k = print_file(file_name);
-                if (k == 0) {
-                    cout << "Empty file\n";
-                }
-                if (k < 0) {
-                    cout << "Can't read file\n";
-                }
+                if (k == 0) std::cout << "Empty file\n";
+
+                if (k < 0) std::cout << "Can't read file\n";
+
                 break;
             }
             case 3: {
-                cout << "file name?";
-                cin >> file_name;
+                std::cout << "file name? ";
+                std::cin >> file_name;
                 Pair key;
-                cout << "Key pair (int double):";
-                cin >> key;
+                std::cout << "Key pair (int double): ";
+                std::cin >> key;
                 int k = del_less(file_name, key);
-                if (k < 0) {
-                    cout << "Can't read file\n";
-                }
+                if (k < 0) std::cout << "Can't read file\n";
+
                 break;
             }
             case 4: {
-                cout << "file name?";
-                cin >> file_name;
+                std::cout << "file name? ";
+                std::cin >> file_name;
                 Pair key;
-                cout << "Key pair (int double):";
-                cin >> key;
-                cout << "Constant type (1-int, 2-double)?";
+                std::cout << "Key pair (int double): ";
+                std::cin >> key;
+                std::cout << "Constant type (1-int, 2-double)? ";
                 int kind = 0;
-                cin >> kind;
+                std::cin >> kind;
                 int count = 0;
                 if (kind == 1) {
                     int l;
-                    cout << "L?";
-                    cin >> l;
+                    std::cout << "L? ";
+                    std::cin >> l;
                     count = inc_equal(file_name, key, l, true);
                 } else {
                     double l;
-                    cout << "L?";
-                    cin >> l;
+                    std::cout << "L? ";
+                    std::cin >> l;
                     count = inc_equal(file_name, key, l, false);
                 }
-                if (count < 0) {
-                    cout << "Can't read file\n";
-                }
+                if (count < 0) std::cout << "Can't read file\n";
+
                 break;
             }
             case 5: {
-                cout << "file name?";
-                cin >> file_name;
+                std::cout << "file name? ";
+                std::cin >> file_name;
                 int n = 0;
                 int k = 0;
-                cout << "N?";
-                cin >> n;
-                cout << "K?";
-                cin >> k;
+                std::cout << "N? ";
+                std::cin >> n;
+                std::cout << "K? ";
+                std::cin >> k;
                 int added = add_after(file_name, n, k);
-                if (added < 0) {
-                    cout << "Can't read file\n";
-                }
+                if (added < 0) std::cout << "Can't read file\n";
+
                 break;
             }
             default:
